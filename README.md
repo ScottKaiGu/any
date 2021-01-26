b3add04d34b37835cd8e03ad9a1cedb9bab0bfb1
gukai <gukai@xiaomi.com>
2021-01-26 23:35:26 +0800

    update README.md to add refs
    Signed-off-by: gukai <gukai@xiaomi.com>

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # any
-blade build # :) 
+
+# ref:
+#    http://rapidjson.org/zh-cn/md_doc_tutorial_8zh-cn.html
+#    https://gotask.net/programming/serialize-and-deserialize-array-in-cpp-using-rapidjson/
+#    https://gotask.net/programming/serialize-and-deserialize-object-in-cpp-using-rapidjson/
+#    https://stackoverflow.com/questions/31383852/parsing-object-inside-array-in-rapidjso
+
+blade build 
+
+# :) 
--- test.cpp
+++ test.cpp
@@ -4,11 +4,6 @@
 using namespace std;
 using namespace rapidjson;
 
-//http://rapidjson.org/zh-cn/md_doc_tutorial_8zh-cn.html
-//https://gotask.net/programming/serialize-and-deserialize-array-in-cpp-using-rapidjson/
-//https://gotask.net/programming/serialize-and-deserialize-object-in-cpp-using-rapidjson/
-//https://stackoverflow.com/questions/31383852/parsing-object-inside-array-in-rapidjson
-
 std::string dump(const Any& any)
 {
     StringBuffer s;
